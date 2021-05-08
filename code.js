476ccbb52a797239a5b051597db2b0db76c93f70
Ivan Tsapko <icdi7434@gmail.com>
2021-05-08 21:09:25 +0300

    Сверстал последнюю страницу, переписал header в бэм

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 function hideMenu() {
-    document.querySelector(".hidden-menu").classList.add("hidden");
+    document.querySelector(".b-header__navMenu").classList.add("b-header__navMenu_hidden");
     console.log("hidden");
 }
 
 function showMenu() {
-    document.querySelector(".hidden-menu").classList.remove("hidden")
+    document.querySelector(".b-header__navMenu").classList.remove("b-header__navMenu_hidden")
     console.log("visible");
 }
